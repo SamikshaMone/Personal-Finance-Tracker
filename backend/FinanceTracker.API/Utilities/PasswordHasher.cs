de5530b187129b8ebb786adad6108f9ca32cff87
Samiksha Mone <samikshamone6@gmail.com>
2025-06-08 23:00:07 +0530

    Update PasswordHasher.cs

--- backend/FinanceTracker.API/Utilities/PasswordHasher.cs
+++ backend/FinanceTracker.API/Utilities/PasswordHasher.cs
@@ -1,1 +1,40 @@
+using Microsoft.AspNetCore.Cryptography.KeyDerivation;
+using System;
+using System.Security.Cryptography;
+
+namespace FinanceTracker.API.Utilities
+{
+    public static class PasswordHasher
+    {
+        public static string HashPassword(string password, out byte[] salt)
+        {
+            salt = new byte[128 / 8];
+            using (var rng = RandomNumberGenerator.Create())
+            {
+                rng.GetBytes(salt);
+            }
+
+            var hash = Convert.ToBase64String(KeyDerivation.Pbkdf2(
+                password: password,
+                salt: salt,
+                prf: KeyDerivationPrf.HMACSHA256,
+                iterationCount: 10000,
+                numBytesRequested: 256 / 8));
+
+            return hash;
+        }
+
+        public static bool VerifyPassword(string password, string storedHash, byte[] salt)
+        {
+            var hash = Convert.ToBase64String(KeyDerivation.Pbkdf2(
+                password: password,
+                salt: salt,
+                prf: KeyDerivationPrf.HMACSHA256,
+                iterationCount: 10000,
+                numBytesRequested: 256 / 8));
+
+            return hash == storedHash;
+        }
+    }
+}
 
