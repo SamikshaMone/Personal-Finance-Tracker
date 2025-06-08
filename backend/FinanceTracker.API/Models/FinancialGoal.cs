50ff432feae9374025afe011b5f6d3ba06d8537c
Samiksha Mone <samikshamone6@gmail.com>
2025-06-08 22:46:51 +0530

    Update FinancialGoal.cs

--- backend/FinanceTracker.API/Models/FinancialGoal.cs
+++ backend/FinanceTracker.API/Models/FinancialGoal.cs
@@ -1,1 +1,28 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FinanceTracker.API.Models
+{
+    public class FinancialGoal
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string GoalName { get; set; }
+
+        [Required]
+        public decimal TargetAmount { get; set; }
+
+        public decimal SavedAmount { get; set; }
+
+        public DateTime TargetDate { get; set; }
+
+        [Required]
+        public string UserId { get; set; }
+
+        [ForeignKey("UserId")]
+        public User User { get; set; }
+    }
+}
 
