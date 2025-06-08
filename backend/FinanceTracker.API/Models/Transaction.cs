365e0d676b2e4a5504a82a16c167029926b5152b
Samiksha Mone <samikshamone6@gmail.com>
2025-06-08 22:47:07 +0530

    Update Transaction.cs

--- backend/FinanceTracker.API/Models/Transaction.cs
+++ backend/FinanceTracker.API/Models/Transaction.cs
@@ -1,1 +1,32 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FinanceTracker.API.Models
+{
+    public class Transaction
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Type { get; set; } // "Income" or "Expense"
+
+        [Required]
+        public string Category { get; set; }
+
+        [Required]
+        public decimal Amount { get; set; }
+
+        [Required]
+        public DateTime Date { get; set; }
+
+        public string Notes { get; set; }
+
+        [Required]
+        public string UserId { get; set; }
+
+        [ForeignKey("UserId")]
+        public User User { get; set; }
+    }
+}
 
