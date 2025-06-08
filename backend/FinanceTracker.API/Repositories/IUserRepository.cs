a7d8e0e61ba590bf824ff7aae8da24f19565947d
Samiksha Mone <samikshamone6@gmail.com>
2025-06-08 22:50:44 +0530

    Update IUserRepository.cs

--- backend/FinanceTracker.API/Repositories/IUserRepository.cs
+++ backend/FinanceTracker.API/Repositories/IUserRepository.cs
@@ -1,1 +1,12 @@
+using FinanceTracker.API.Models;
+using System.Threading.Tasks;
+
+namespace FinanceTracker.API.Repositories
+{
+    public interface IUserRepository
+    {
+        Task<User> GetUserByIdAsync(string userId);
+        Task<bool> UserExistsAsync(string userId);
+    }
+}
 
