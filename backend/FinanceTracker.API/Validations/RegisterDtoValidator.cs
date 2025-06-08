35da481796240ecf943c430af8727831a711f6fb
Samiksha Mone <samikshamone6@gmail.com>
2025-06-08 23:04:03 +0530

    Update RegisterDtoValidator.cs

--- backend/FinanceTracker.API/Validations/RegisterDtoValidator.cs
+++ backend/FinanceTracker.API/Validations/RegisterDtoValidator.cs
@@ -1,1 +1,27 @@
+using FluentValidation;
+using FinanceTracker.API.DTOs;
+
+namespace FinanceTracker.API.Validations
+{
+    public class RegisterDtoValidator : AbstractValidator<RegisterDto>
+    {
+        public RegisterDtoValidator()
+        {
+            RuleFor(x => x.UserName)
+                .NotEmpty().WithMessage("Username is required")
+                .MinimumLength(3).WithMessage("Username must be at least 3 characters");
+
+            RuleFor(x => x.Email)
+                .NotEmpty().WithMessage("Email is required")
+                .EmailAddress().WithMessage("A valid email is required");
+
+            RuleFor(x => x.Password)
+                .NotEmpty().WithMessage("Password is required")
+                .MinimumLength(6).WithMessage("Password must be at least 6 characters");
+
+            RuleFor(x => x.ConfirmPassword)
+                .Equal(x => x.Password).WithMessage("Passwords must match");
+        }
+    }
+}
 
